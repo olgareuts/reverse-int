61db19001eb843db5cbbe2c110d496962d09ffd9
olgareuts <olga.re@mail.ru>
2023-01-05 23:52:47 +0300

    init: start revers-int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+  let reverseNumber = 0;
+  let numberValue = Math.abs(n);
+  while (numberValue) {
+    reverseNumber = reverseNumber * 10 + numberValue % 10; 
+    numberValue = Math.floor(numberValue / 10);
+  }
+  return reverseNumber;
 }
+
